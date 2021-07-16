bef41af9b05c57f4faf089019a4eeef29765c4cd
Sh4ke96 <Sh4ke96@gmail.com>
2021-07-16 02:42:08 +0200

    add result box show after all questions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const start_btn = document.querySelector(".start__btn");
 const info_box = document.querySelector(".info__box");
+const quiz_box = document.querySelector(".quiz__box");
 const exit_btn = info_box.querySelector(".quit__btn");
-const continue_btn = info_box.querySelector(".restart__btn");
+const continue_btn = info_box.querySelector(".continue__btn");
 const que_options = document.querySelector(".section__options-list");
+const timeCount = document.querySelector(".time__text");
 
 start_btn.onclick = () => {
   info_box.style.display = "block";
@@ -13,25 +15,54 @@ exit_btn.onclick = () => {
 };
 
 continue_btn.onclick = () => {
-  const quiz_box = document.querySelector(".quiz__box");
   quiz_box.style.display = "block";
   info_box.style.display = "none";
   showQuestions(0);
   queCounter(1);
+  startTimer(10);
 };
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let timeValue = 10;
+let userScore = 0;
 
 const next_btn = document.querySelector(".next__btn");
+const result_box = document.querySelector(".result__box");
+const restart_btn = result_box.querySelector(".restart__btn");
+const quit_btn = result_box.querySelector(".quit__btn");
+
+restart_btn.onclick = () => {
+  quiz_box.style.display = "block";
+  result_box.style.display = "none";
+  let que_count = 0;
+  let que_numb = 1;
+  let timeValue = 10;
+  let userScore = 0;
+  showQuestions(que_count);
+  queCounter(que_numb);
+  clearInterval(counter);
+  startTimer(timeValue);
+  next_btn.style.display = "none";
+};
+
+quit_btn.onclick = () => {
+  window.location.reload();
+};
+
 next_btn.onclick = () => {
   if (que_count < questions.length - 1) {
     que_count++;
     que_numb++;
     showQuestions(que_count);
     queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    next_btn.style.display = "none";
   } else {
-    console.log("Questions completed");
+    clearInterval(counter);
+    showResultBox();
   }
 };
 
@@ -65,10 +96,12 @@ function showQuestions(index) {
 }
 
 function optionSelected(answer) {
+  clearInterval(counter);
   let userAnswer = answer.textContent;
   let correctAnswer = questions[que_count].answer;
   let allOptions = que_options.children.length;
   if (userAnswer == correctAnswer) {
+    userScore += 1;
     answer.classList.add("correct");
   } else {
     answer.classList.add("wrong");
@@ -81,6 +114,7 @@ function optionSelected(answer) {
   for (let i = 0; i < allOptions; i++) {
     que_options.children[i].classList.add("disabled");
   }
+  next_btn.style.display = "block";
 }
 
 function queCounter(index) {
@@ -92,4 +126,52 @@ function queCounter(index) {
     questions.length +
     "</span> Questions</p>";
   bottom_que.innerHTML = totalQueCountTag;
+}
+
+function showResultBox() {
+  quiz_box.style.display = "none";
+  info_box.style.display = "none";
+  result_box.style.display = "flex";
+  const scoreText = result_box.querySelector(".complete__score");
+  if (userScore > 5) {
+    let scoreTag =
+      'Congratulation! You got <span class="complete__score-color">' +
+      userScore +
+      '</span> out of <span class="complete__score-color">' +
+      questions.length +
+      "</span> points.";
+    scoreText.innerHTML = scoreTag;
+  } else if (userScore > 8) {
+    let scoreTag =
+      'Awesome!, You got <span class="complete__score-color">' +
+      userScore +
+      '</span> out of <span class="complete__score-color">' +
+      questions.length +
+      "</span> points.";
+    scoreText.innerHTML = scoreTag;
+  } else {
+    let scoreTag =
+      'Sorry, You got <span class="complete__score-color">' +
+      userScore +
+      '</span> out of <span class="complete__score-color">' +
+      questions.length +
+      "</span> points.";
+    scoreText.innerHTML = scoreTag;
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "Time's up";
+    }
+  }
 }
